347b95fc3707cf31b57386c0755478ce5b2711f6
sonimohit481 <sonimohit481@gmail.com>
2022-12-07 21:16:49 +0530

    small changes

---
+++ src/Components/DisplayProduct.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const DisplayProduct = () => {
+  return <div>DisplayProduct</div>;
+};
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { link } from "react-router-dom";
+
+export const NavBar = () => {
+  return <div></div>;
+};
